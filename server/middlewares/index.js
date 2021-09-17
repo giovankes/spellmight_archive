2b77723522175b5c3f57ac45a06654b2e5708ddf
giotje <devgio@icloud.com>
2021-09-17 14:02:45 +0200

    removed old server

--- server/middlewares/index.js
+++
@@ -1,2 +0,0 @@
-export { default as authenticated, verifyToken } from './authenticated.js'
-export { handleError, ErrorHandler } from './error.js'
--- server/models/roomsModel.js
+++
@@ -1,7 +0,0 @@
-
-import mongoose from 'mongoose';
-import roomsSchema from '../schema/rooms.js';
-
-const roomsModel = mongoose.model('rooms', roomsSchema);
-
-export default roomsModel;
