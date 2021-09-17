1926d530a2884f1571cd302d60deaa04602dd6e5
giotje <devgio@icloud.com>
2021-09-17 17:03:54 +0200

    you can now connect to the socket.io server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const server = require('express')()
 const http = require('http').createServer(server)
-const io = require('socket.io')(http)
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+  },
+})
+
+let players = []
 const consola = require('consola')
 io.on('connection', function (socket) {
-  console.log('A user connected: ' + socket.id)
+  consola.info('A user connected: ' + socket.id)
+  players.push(socket.id)
 
+  if (players.length === 1) {
+    io.emit('isPlayerA')
+  }
   socket.on('disconnect', function () {
-    consola.success('A user disconnected: ' + socket.id)
+    consola.info('A user disconnected: ' + socket.id)
   })
 })
 
