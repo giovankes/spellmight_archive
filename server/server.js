9a3892fd3b27d34b83f34ecaadc1a44dde2314d7
giotje <devgio@icloud.com>
2021-09-17 15:54:09 +0200

    implemented server file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const server = require('express')()
 const http = require('http').createServer(server)
 const io = require('socket.io')(http)
-
+const consola = require('consola')
 io.on('connection', function (socket) {
   console.log('A user connected: ' + socket.id)
 
   socket.on('disconnect', function () {
-    console.log('A user disconnected: ' + socket.id)
+    consola.success('A user disconnected: ' + socket.id)
   })
 })
 
-http.listen(8080, function () {
-  console.log('Server started!')
+http.listen(8081, function () {
+  consola.success('Server started!')
 })
