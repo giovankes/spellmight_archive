21b5300783a4f9d1624cff2998904cd867ad3593
Giotje <devgio@icloud.com>
2022-01-08 20:38:58 +0100

    imported file from other directory

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,6 @@
-use bevy::prelude::*;
-struct Person;
-struct Name(String);
-pub struct HelloPlugin;
-struct GreetTimer(Timer);
-fn hello_world() {
-    println!("hello world")
-}
+#[path = "components/hallo.rs"] mod hello;
 
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Gio".to_string()));
-}
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(hello_world.system())
-            .add_system(greet_people.system());
-    }
-}
 fn main() {
-    App::build()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-        .run();
+  hello::hello(); 
 }
